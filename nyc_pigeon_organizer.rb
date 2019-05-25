22ed1b89e4111265700e949278fc6bce54473696
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-25 16:29:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  nhash = {}
+  hash = {}
 
   data.each do |key, val|
     val.each do |info, arr|
-      arr.each do |
-
+      arr.each do |name|
+        if hash.include?(name)
+          
+        else
+          hash[name] = {key => [info.to_s]}
       end
     end
   end
